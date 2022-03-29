800e7efc221d765b2774bbb3019eb90a3df36b57
Biloliddin <najot.6162@gmail.com>
2022-03-29 21:37:45 +0500

    added auth api

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use \App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,13 +19,24 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/posts',[Postcontroller::class,'index']);
 
-Route::post('/post',[Postcontroller::class,'store']);
 
-Route::get('/posts/{id}',[Postcontroller::class,'show']);
+Route::post('/register',[AuthController::class,'register']);
 
-Route::put('/posts/{id}',[Postcontroller::class,'update']);
+Route::post('/login',[AuthController::class,'login']);
 
-Route::delete('/posts/{id}',[Postcontroller::class,'destroy']);
+Route::group(['middleware' => ['auth:sanctum']],function (){
 
+    Route::get('/posts',[Postcontroller::class,'index']);
+
+    Route::post('/post',[Postcontroller::class,'store']);
+
+    Route::get('/posts/{id}',[Postcontroller::class,'show']);
+
+    Route::put('/posts/{id}',[Postcontroller::class,'update']);
+
+    Route::delete('/posts/{id}',[Postcontroller::class,'destroy']);
+
+    Route::post('/logout',[AuthController::class, 'logout']);
+
+});
